9ca7bab96edea4e6e91910a98f1cce2f436b8cbe
JMoney-216 <jdm216@gmail.com>
2020-01-06 16:58:54 -0600

    creates realtime circle game

---
+++ io.js
@@ -0,0 +1,28 @@
+// io.js
+
+var io = require("socket.io")();
+
+var players = {};
+
+// Listen for new connections from clients (socket)
+io.on("connection", function(socket) {
+  // console.log("Client connected to socket.io!");
+  socket.on("register-player", function(initials) {
+    players[socket.id] = initials;
+    io.emit("update-player-list", Object.values(players));
+  });
+  socket.on("disconnect", function() {
+    delete players[socket.id];
+    io.emit("update-player-list", Object.values(players));
+  });
+
+  socket.on("add-circle", function(data) {
+    io.emit("add-circle", data);
+  });
+  socket.on("clearCircles", function() {
+    io.emit("clearCircles");
+  });
+});
+
+// io represents socket.io on the server - let's export it
+module.exports = io;
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,33 +1,66 @@
-var circles = document.getElementById('circles');
-var initials = '';
+var socket = io();
 
-circles.addEventListener('click', function(evt) {
-  addCircle(evt.clientX, evt.clientY, randomBetween(10,125), getRandomRGBA());
+// listen to the server for the `add-circle` event
+socket.on("add-circle", function(data) {
+  addCircle(data);
+  // console.log(data);
+});
+socket.on("clearCircles", function() {
+  clearCircles();
+});
+socket.on("update-player-list", function(data) {
+  var playerList = "<li>" + data.join("</li><li>") + "</li>";
+  players.innerHTML = playerList;
 });
 
-document.getElementsByTagName('button')[0].addEventListener('click', function() {
-  circles.innerHTML = '';
+var circles = document.getElementById("circles");
+var players = document.getElementById("players");
+let clear = document.getElementById("clear");
+var initials = "";
+
+window.onload = function() {
+  let color = getRandomRGBA();
+  document.body.style.backgroundColor = color;
+};
+
+circles.addEventListener("click", function(evt) {
+  socket.emit("add-circle", {
+    initials: initials,
+    x: evt.clientX,
+    y: evt.clientY,
+    dia: randomBetween(20, 130),
+    rgba: getRandomRGBA()
+  });
 });
+
+clear.addEventListener("click", function() {
+  socket.emit("clearCircles");
+});
+
+function clearCircles() {
+  circles.innerHTML = "";
+}
 
 do {
   initials = getInitials();
 } while (initials.length < 2 || initials.length > 3);
+socket.emit("register-player", initials);
 
 function getInitials() {
   var input = prompt("Please enter your initials");
-  return input ? input.toUpperCase() : '';
+  return input ? input.toUpperCase() : "";
 }
 
-function addCircle(x, y, dia, rgba) {
-  var el = document.createElement('div');
-  el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
-  el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
-  el.style.width = el.style.height = dia + 'px';
+function addCircle({ x, y, dia, rgba, initials }) {
+  var el = document.createElement("div");
+  el.style.left = x - Math.floor(dia / 2 + 0.5) + "px";
+  el.style.top = y - Math.floor(dia / 2 + 0.5) + "px";
+  el.style.width = el.style.height = dia + "px";
   el.style.backgroundColor = rgba;
-  el.style.fontSize = Math.floor(dia / 3) + 'px';
-  el.style.color = 'white';
-  el.style.textAlign = 'center';
-  el.style.lineHeight = dia + 'px';
+  el.style.fontSize = Math.floor(dia / 3) + "px";
+  el.style.color = "white";
+  el.style.textAlign = "center";
+  el.style.lineHeight = dia + "px";
   el.innerHTML = initials;
   circles.appendChild(el);
 }
@@ -37,6 +70,15 @@ function randomBetween(min, max) {
 }
 
 function getRandomRGBA() {
-  return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
-    randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
+  return [
+    "rgba(",
+    randomBetween(0, 255),
+    ",",
+    randomBetween(0, 255),
+    ",",
+    randomBetween(0, 255),
+    ",",
+    randomBetween(2, 10) / 10,
+    ")"
+  ].join("");
 }
